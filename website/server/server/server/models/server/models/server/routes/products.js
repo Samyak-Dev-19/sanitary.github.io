afa049f5019832bb54ea56702c35840653001269
Samyak-Dev-19 <samyakkatarne07@gmail.com>
2026-03-02 13:59:37 +0530

    Delete website directory

--- website/server/server/server/models/server/models/server/routes/products.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { Product } from "../models/Product.js";
-
-export const productsRouter = express.Router();
-
-productsRouter.get("/", async (req, res) => {
-  const products = await Product.find({ active: true }).sort({ createdAt: -1 });
-  res.json(products);
-});
